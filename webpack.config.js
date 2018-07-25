37682e1aa77cbe50876f06f8322c8bf8feff6164
michael tamsil <michaeltamsil@gmail.com>
2018-07-25 13:11:11 +0700

    update package.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,21 +16,7 @@ module.exports = () => ({
   },
   devtool: '#eval-source-map',
   plugins: [
-    new HotModuleReplacementPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      compress: {
-        warnings: false, // Suppress uglification warnings
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-      },
-      exclude: [/\.min\.js$/gi] // skip pre-minified libs
-    })
+    new HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
