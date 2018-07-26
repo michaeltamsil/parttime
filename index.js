3ddb2d2008c1d8004b27cda885343e7f35260e4b
michael <michaeltamsil@gmail.com>
2018-07-26 16:43:44 +0700

    use bundle js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<div><BrowserRouter><Routes /></BrowserRouter></div>, document.getElementById('root'));
+
+module.hot.accept();
